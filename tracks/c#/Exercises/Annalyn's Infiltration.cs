ee55b62c59668ac3f64ed0ce916e72aa467bc6d4
Hüseyin Yılmaz <74953321+yilmazhuseyin@users.noreply.github.com>
2022-07-28 11:33:20 +0300

    Update Annalyn's Infiltration.cs

--- tracks/c#/Exercises/Annalyn's Infiltration.cs
+++ tracks/c#/Exercises/Annalyn's Infiltration.cs
@@ -9,22 +9,16 @@ static class QuestLogic
 
     public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
     {
-        return ((knightIsAwake || archerIsAwake) || prisonerIsAwake) ? true : false;
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
     }
 
     public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
     {
-        return (!archerIsAwake && prisonerIsAwake) ? true : false;
+        return !archerIsAwake && prisonerIsAwake;
     }
 
     public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
     {
-        if((!knightIsAwake && archerIsAwake) && ((!prisonerIsAwake && petDogIsPresent) || (prisonerIsAwake && petDogIsPresent) || (prisonerIsAwake && !petDogIsPresent)) )
-            return false;
-
-        if(knightIsAwake  && !archerIsAwake && prisonerIsAwake && !petDogIsPresent)
-            return false;
-        
-        return (((!knightIsAwake && !archerIsAwake) || (knightIsAwake && !archerIsAwake) || (!knightIsAwake && archerIsAwake)) && ((!prisonerIsAwake && petDogIsPresent) || (prisonerIsAwake && petDogIsPresent) || (prisonerIsAwake && !petDogIsPresent))) ? true : false;
+        return (!archerIsAwake && petDogIsPresent) || (!petDogIsPresent && prisonerIsAwake && !archerIsAwake && !knightIsAwake);
     }
 }
