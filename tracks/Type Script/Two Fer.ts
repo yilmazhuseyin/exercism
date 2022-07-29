5ff3331840629203f0615b395880cf41033b6fc9
Hüseyin Yılmaz <74953321+yilmazhuseyin@users.noreply.github.com>
2022-07-29 09:47:07 +0300

    Update Two Fer.ts

--- tracks/Type Script/Two Fer.ts
+++ tracks/Type Script/Two Fer.ts
@@ -2,3 +2,5 @@ export function twoFer(name:string = "you"): string {
     return `One for ${name}, one for me.`;
   }
   
+//type script function with parameter
+//type script function parameter with default value
