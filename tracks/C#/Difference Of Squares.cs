efc87b72ddc04d20637fd6dd841516121708dd37
Hüseyin Yılmaz <74953321+yilmazhuseyin@users.noreply.github.com>
2022-08-01 08:35:34 +0300

    Create  Difference Of Squares.cs

---
+++ tracks/C#/Difference Of Squares.cs
@@ -0,0 +1,28 @@
+using System;
+
+public static class DifferenceOfSquares
+{
+    public static int CalculateSquareOfSum(int max){
+     int sum = 0;
+        for (int i = 1; i <= max; i++)
+        {
+            sum += i;
+        }
+        return (int)Math.Pow(sum, 2);    
+    }
+
+    public static int CalculateSumOfSquares(int max)
+    {
+       int sum = 0;
+        for (int i = 1; i <= max; i++)
+        {
+            sum += (int)Math.Pow(i, 2); ;
+        }
+        return sum;
+    }
+
+    public static int CalculateDifferenceOfSquares(int max)
+    {
+        return CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+    }
+}
