b026caf43fc15dbd465d207c2e9d3aaa10db2a12
Hüseyin Yılmaz <74953321+yilmazhuseyin@users.noreply.github.com>
2022-08-04 11:56:37 +0300

    Update Tim from Marketing.cs

--- tracks/C#/Tim from Marketing.cs
+++ tracks/C#/Tim from Marketing.cs
@@ -10,3 +10,25 @@ static class Badge
             return $"[{id}] - {name} - {department?.ToUpper() ?? "OWNER"}";
     }
 }
+
+static class Badge
+{
+    public static string Print(int? id, string name, string? department)
+    {       
+        string dep = department?.ToUpper();
+        if(id == null) return $"{name} - {dep ?? "OWNER"}";
+        else return $"[{id}] - {name} - {dep ?? "OWNER"}";
+    }
+}
+
+
+static class Badge
+{
+    public static string Print(int? id, string name, string? department)
+    {       
+        string dep = department?.ToUpper();
+        string body = $"{name} - {dep ?? "OWNER"}";
+        if(id == null) return body;
+        else return $"[{id}] - {body}";
+    }
+}
