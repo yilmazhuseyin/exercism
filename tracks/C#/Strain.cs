beb3c055fc02061041c4dbb4e81e776e4542575f
Hüseyin Yılmaz <74953321+yilmazhuseyin@users.noreply.github.com>
2022-08-17 10:10:16 +0300

    Create Strain.cs

---
+++ tracks/C#/Strain.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Strain
+{
+    public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Func<T, bool> predicate) => collection.Where(x=>predicate(x));
+
+    public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Func<T, bool> predicate) => collection.Where(x=>!predicate(x));
+      
+}
