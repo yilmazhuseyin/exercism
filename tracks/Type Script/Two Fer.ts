d5dfab2f63a47830145b6cf86078c29f371c4505
Hüseyin Yılmaz <74953321+yilmazhuseyin@users.noreply.github.com>
2022-08-24 15:20:07 +0300

    Delete tracks/Type Script directory

--- tracks/Type Script/Two Fer.ts
+++
@@ -1,6 +0,0 @@
-export function twoFer(name:string = "you"): string {
-    return `One for ${name}, one for me.`;
-  }
-  
-//type script function with parameter
-//type script function parameter with default value
