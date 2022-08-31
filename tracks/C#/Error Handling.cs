6c833add210558d3cd2ec8f01229d45c9b2355dd
Hüseyin Yılmaz <74953321+yilmazhuseyin@users.noreply.github.com>
2022-08-31 11:55:05 +0300

    Create Error Handling.cs

---
+++ tracks/C#/Error Handling.cs
@@ -0,0 +1,18 @@
+using System;
+
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException() => throw new Exception();
+    
+    public static int? HandleErrorByReturningNullableType(string input) => int.TryParse(input, out int result) ? result : null;
+
+    public static bool HandleErrorWithOutParam(string input, out int result) => int.TryParse(input, out result);
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
+    {
+        using (disposableObject)
+        {
+            throw new Exception();
+        }
+    }
+}
