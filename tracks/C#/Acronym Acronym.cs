7ac2d4bc1ce75f5f2e41c02dfd603017757ac584
Hüseyin Yılmaz <74953321+yilmazhuseyin@users.noreply.github.com>
2022-09-09 14:52:09 +0300

    Create Acronym Acronym.cs

---
+++ tracks/C#/Acronym Acronym.cs
@@ -0,0 +1,33 @@
+using System;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        var result = string.Empty;
+
+        result += phrase[0]; 
+
+        var index = 0; //To avoid repetitive additions
+
+        for (int i = 0; i < phrase.Length; i++)
+        {
+            bool isLetter = char.IsLetter(phrase[i]);
+            bool isapostrophe = phrase[i] == '\'';
+
+            if (!isLetter && !isapostrophe && char.IsLetter(phrase[i + 1]) && index != i + 1)
+            {
+                result += phrase[i + 1];
+                index = i + 1;
+
+            }
+            else if (!isLetter && !isapostrophe && char.IsLetter(phrase[i + 2]) && index != i + 1)
+            {
+                result += phrase[i + 2];
+                index = i + 2;
+            }
+        }
+
+        return result.ToUpper();
+    }
+}
